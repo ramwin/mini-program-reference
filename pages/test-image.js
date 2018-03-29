0294c132645a43939b80a24e6db4482f9fb125bc
Xiang Wang <wangx@windows.shareginepc.ramwin.com>
2018-03-29 18:20:14 +0800

    测试input，添加吐槽

---
+++ pages/test-image.js
@@ -0,0 +1,76 @@
+// pages/time/test-image.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  clickimage: function() {
+    console.log("你点我了")
+    wx.previewImage({
+      current: 1,
+      urls: [
+        "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+        "http://www.ramwin.com/media/qrcode.png",
+      ]
+    });
+  }
+})
