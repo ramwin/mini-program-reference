b0ab590ab0178afa40d2448a41a783c34cc048be
Xiang Wang <ramwin@qq.com>
2018-12-14 12:21:36 +0800

    测试自定义组件的样式

---
+++ pages/frame/custom-component/components/test-style.js
@@ -0,0 +1,26 @@
+// pages/frame/custom-component/components/test-style.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  },
+  externalClasses: ['my-color'],
+  options: {
+    addGlobalClass: true,
+  },
+})
