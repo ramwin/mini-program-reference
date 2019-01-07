b76d5a0958352c09503cfd7a017cc6ac55347223
Xiang Wang <ramwin@qq.com>
2019-01-07 09:19:44 +0800

    组件可以有margin和padding

---
+++ pages/frame/custom-component/components/component-padding.js
@@ -0,0 +1,26 @@
+// pages/frame/custom-component/components/component-padding.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+  options: {
+    addGlobalClass: true,
+  },
+})
