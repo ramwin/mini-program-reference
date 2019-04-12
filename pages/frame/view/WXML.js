dfe63feaab01ab70f2438a63d835be5097350513
Xiang Wang <ramwin@qq.com>
2019-04-12 18:34:00 +0800

    尝试阻止事件冒泡

--- pages/frame/view/WXML.js
+++ pages/frame/view/WXML.js
@@ -1,9 +1,5 @@
 // pages/frame/view/WXML.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     complex_data: {
       list: [1,2,3],
@@ -12,62 +8,55 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   },
   tapName: function(event) {
     console.log(event);
+  },
+  touchmove: function(evt) {
+    console.info("摸了", evt)
+  },
+  touchmove_returnfalse: function(evt) {
+    console.info(evt)
+    return false;
+  },
+  catch_false: function(evt) {
+    return false
+  },
+  catch_true: function(evt) {
+    console.info("catch_true")
+    return true;
+  },
+  bind_raise: function(evt) {
+    throw Error("你的蒙版，你不能滚");
   }
 })
