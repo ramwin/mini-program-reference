c21273b7972c5655cb16173efe1659c72f33d38a
Xiang Wang <ramwin@qq.com>
2019-10-08 14:51:35 +0800

    测试

---
+++ pages/component/media/video.js
@@ -0,0 +1,14 @@
+// pages/component/media/video.js
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  },
+  test: function() {
+    wx.showToast({
+      "title": "点我啦",
+    })
+  },
+})
